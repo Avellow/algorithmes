6838f160c20dcfc23895d1ea8a0840e789175f20
Avellow <avellow@yandex.ru>
2023-03-17 12:29:08 +0300

    added recursive binary search variant

--- simple/SearchAlgorithms/binary.js
+++ simple/SearchAlgorithms/binary.js
@@ -30,3 +30,32 @@ console.log(binary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], -5)); // 11 items
 console.log(binary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], 156));
 console.log(binary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], 2));
 console.log(binary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], 200));
+console.log("____________________");
+
+function recursiveBinary(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
+}
+
+function search(arr, target, leftIndex, rightIndex) {
+  if (leftIndex > rightIndex) {
+    return -1;
+  }
+  const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+  const element = arr[middleIndex];
+
+  if (element === target) {
+    return middleIndex;
+  } else if (element < target) {
+    return search(arr, target, middleIndex + 1, rightIndex);
+  } else {
+    return search(arr, target, leftIndex, middleIndex - 1);
+  }
+}
+
+console.log(recursiveBinary([], 0));
+console.log(recursiveBinary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], -5)); // 11 items
+console.log(recursiveBinary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], 156));
+console.log(recursiveBinary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], 2));
+console.log(recursiveBinary([-5, -2, 1, 2, 3, 4, 14, 18, 29, 100, 156], 200));
+console.log("____________________");
